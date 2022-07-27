1dffae24f53b8d10025e27562ca60407691c5b83
Qrowio <66504185+Qrowio@users.noreply.github.com>
2022-07-27 23:33:56 +0100

    Updated edit
    Removes 1 from the 'blogs' row on the user table when a blog is deleted. Don't know why this didn't already happen but it does now.

--- classes/Edit.class.php
+++ classes/Edit.class.php
@@ -6,6 +6,7 @@ class Edit extends Database {
     private $row;
     private $title;
     private $description;
+    private $userID;
 
     function info() {
         return $this->row;
@@ -13,13 +14,15 @@ class Edit extends Database {
     
     function __construct() {
         parent::__construct();
+        $this->userID = $_SESSION['blogger']['id'];
         $this->sql = $this->connection->prepare("SELECT * FROM blogs WHERE id = :id");
         $this->sql->execute([':id' => $_GET['id']]); 
         $this->row = $this->sql->fetch(PDO::FETCH_ASSOC);
         if(isset($_POST['delete'])){
             $this->image = $this->row['image'];
-            echo $this->image;
             unlink($this->image);
+            $this->sql = $this->connection->prepare("UPDATE user SET blogs = blogs - 1 WHERE id = :id");
+            $this->sql->execute([':id' => $this->userID]);
             $this->sql = $this->connection->prepare("DELETE FROM blogs WHERE id = :id");
             $this->sql->execute([':id' => $_GET['id']]); 
             $this->row = $this->sql->fetch(PDO::FETCH_ASSOC);
